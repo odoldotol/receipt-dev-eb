84f400d629f9a0092ef93760ee4cdd4b8911ae54
odoldotol <odoldotol@gmail.com>
2022-08-17 14:36:07 +0900

    feat: uploadRecipt

--- src/recipt-to-sheet/recipt-to-sheet.controller.ts
+++ src/recipt-to-sheet/recipt-to-sheet.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ReciptToSheetService } from './recipt-to-sheet.service';
 
 @Controller('recipt-to-sheet')
 export class ReciptToSheetController {
-    
+    constructor(private readonly reciptToSheetService: ReciptToSheetService) {}
+
+    @Post()
+    @UseInterceptors(FileInterceptor('reciptImage'/*, {options} */))
+    async uploadRecipt(@UploadedFile() reciptImage: Express.Multer.File) {
+        return this.reciptToSheetService.uploadRecipt(reciptImage);
+    }
 }
--- src/recipt-to-sheet/recipt-to-sheet.service.ts
+++ src/recipt-to-sheet/recipt-to-sheet.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ReciptToSheetService {}
+export class ReciptToSheetService {
+
+    async uploadRecipt(reciptImage: Express.Multer.File) {
+
+        // AWS 에 이미지 업로드 (optional)
+        // 구글 비젼 API 에 업로드
+        /* 데이터 추출
+            어디 영수증인지 알아내기 -> 이 부분 무시하고 홈플러스 라고 가정
+            필요한 문자열들만 추출 (마트별 솔루션 개발 필요) -> 홈플러스 솔루션 적용
+        */
+        // 몽고디비에 저장하기 (optional)
+        // Sheet 만들기
+        // Sheet file 응답
+
+        console.log(reciptImage);
+        return;
+    }
+}
