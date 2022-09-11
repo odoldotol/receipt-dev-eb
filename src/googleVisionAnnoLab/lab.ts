009c970d2eb57b3fa6fb2b87b463f32dcb8a4919
odoldotol <odoldotol@gmail.com>
2022-09-11 12:49:19 +0900

    feat: save fulltextAnnoPlusStudy

--- src/googleVisionAnnoLab/lab.ts
+++ src/googleVisionAnnoLab/lab.ts
@@ -1,18 +1,23 @@
 import getReceiptObject from '../receiptObj/get.V0.0.1';
 import googleVisionAnnoInspectorPipe from '../googleVisionAnnoPipe/inspector.V0.0.1';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFile } from 'fs';
 
 /* ------------------------------------------------------------------ */
 const receiptStyle = "homeplus"; //
-const receiptNumber = 1; //
+const receiptNumber = 5; //
 /* ------------------------------------------------------------------ */
 
 const annotateResult = JSON.parse(readFileSync(`src/googleVisionAnnoLab/annotateResult/${receiptStyle}/${receiptNumber}.ts`, 'utf8').slice(9));
 
 const multipartBody = JSON.parse(readFileSync(`src/googleVisionAnnoLab/annotateResult/${receiptStyle}/${receiptNumber}-body.ts`, 'utf8').slice(9));
 
-const receiptObject = getReceiptObject(
-    googleVisionAnnoInspectorPipe(annotateResult),
-    multipartBody
-);
+const pipedAnnotateResult = googleVisionAnnoInspectorPipe(annotateResult);
+
+/* fullTextAnnotationPlusStudy 파일 쓰기
+const data = "export = " + JSON.stringify(pipedAnnotateResult.fullTextAnnotationPlusStudy, null, 4);
+writeFile(`src/googleVisionAnnoLab/fullTextAnnotationPlusStudy/${receiptStyle}/${receiptNumber}.ts`, data, () => { console.log("WRITED: a fullTextAnnotationPlusStudy file"); });
+*/
+
+const receiptObject = getReceiptObject(pipedAnnotateResult, multipartBody);
+
 console.log(receiptObject);
