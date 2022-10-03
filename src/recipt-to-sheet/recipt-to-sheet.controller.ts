cb9ccb4f62a6d6e18202e60bfa842a0cb1bb177a
odoldotol <odoldotol@gmail.com>
2022-10-03 13:05:32 +0900

    patch: deploy-eb

--- src/recipt-to-sheet/recipt-to-sheet.controller.ts
+++ src/recipt-to-sheet/recipt-to-sheet.controller.ts
@@ -4,7 +4,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { MultipartBodyDto } from './dto/multipartBody.dto';
 import { ReciptToSheetService } from './recipt-to-sheet.service';
 
-@Controller('recipt-to-sheet')
+@Controller('receipt-to-sheet')
 export class ReciptToSheetController {
     constructor(
         private readonly configService: ConfigService,
@@ -12,7 +12,7 @@ export class ReciptToSheetController {
     ) {}
     
     @Post()
-    @UseInterceptors(FileInterceptor('reciptImage'/*, {options} */))
+    @UseInterceptors(FileInterceptor('receiptImage'/*, {options} */))
     async processingTransferredReceipt(@UploadedFile() reciptImage: Express.Multer.File, @Body() multipartBody: MultipartBodyDto) { // 지금은 단일 이미지만 처리한다. 추후에는 여러 영수증이미지를 받아서 처리할 수 있도록 하자.
         if (multipartBody.password !== this.configService.get('Temporary_PASSWORD')) {
             throw new UnauthorizedException();
@@ -25,7 +25,7 @@ export class ReciptToSheetController {
     };
 
     // @Post('lab')
-    // @UseInterceptors(FileInterceptor('reciptImage'/*, {options} */))
+    // @UseInterceptors(FileInterceptor('receiptImage'/*, {options} */))
     // sendGoogleVisionAnnotateResultToLabs(@UploadedFile() reciptImage: Express.Multer.File, @Body() multipartBody: MultipartBodyDto) {
     //     return this.reciptToSheetService.sendGoogleVisionAnnotateResultToLabs(reciptImage, multipartBody);
     // };
